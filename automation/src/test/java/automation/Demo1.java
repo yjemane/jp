3ef1f71e799513de2d33836e7ce454623cebdb0b
yjema <yjema@New2023.lan>
2023-01-15 19:30:05 -0600

    second file modified

--- automation/src/test/java/automation/Demo1.java
+++ automation/src/test/java/automation/Demo1.java
@@ -5,6 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("hello yared");
+       System.out.println("hello yared mf");
 	}
 
 }
